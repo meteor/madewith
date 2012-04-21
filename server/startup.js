32219bb0aaa61b85b0a7957a312b29cd2388093c
matt debergalis <deberg@gmail.com>
2012-04-21 09:41:24 -0700

    spelling

--- server/startup.js
+++ server/startup.js
@@ -1,7 +1,7 @@
 // Disable default mutator methods. Instead clients must use explicit methods as defined
 // in methods.js.
 Meteor.startup(function() {
-  _.each(['mdewith_apps', 'madewith_comments'], function(collection) {
+  _.each(['madewith_apps', 'madewith_comments'], function(collection) {
     _.each(['insert', 'update', 'remove'], function(method) {
       Meteor.default_server.method_handlers['/' + collection + '/' + method] = function() {};
     });
